89b76aaa670323d14cf2e4ec3ce193453bd2b28f
Eder Silva <gunder1eder@gmail.com>
2026-03-03 12:49:34 -0300

    Fix PORT for production (Render)

--- server.js
+++ server.js
@@ -3,16 +3,21 @@ import path from "path";
 import tasksRoutes from "./src/routes/tasks.routes.js";
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
 
 // API
 app.use("/api/tasks", tasksRoutes);
 
-// Front (estático)
+// Frontend
 app.use(express.static(path.join(process.cwd(), "public")));
 
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(process.cwd(), "public", "index.html"));
+});
+
 app.listen(PORT, () => {
-  console.log(`Servidor rodando em http://localhost:${PORT}`);
+  console.log(`Servidor rodando na porta ${PORT}`);
 });
